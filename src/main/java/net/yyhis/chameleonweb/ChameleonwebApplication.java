4135ad3a95ffd1ea0753cefb6bdcdde38346e4c9
yyhis <oouoo717@gmail.com>
2025-03-24 12:06:41 +0900

    init: 초기 세팅 및 초기화

---
+++ src/main/java/net/yyhis/chameleonweb/ChameleonwebApplication.java
@@ -0,0 +1,13 @@
+package net.yyhis.chameleonweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChameleonwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChameleonwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yyhis/chameleonweb/ChameleonwebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yyhis.chameleonweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChameleonwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
